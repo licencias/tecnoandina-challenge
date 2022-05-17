bfd29242611e2ae7a8eec822a6a0df293cd9514d
Michael <michael.ferdinand.sson@gmail.com>
2022-05-18 00:04:35 +0200

    feat(backend):servicio-3

--- servicio-2/app.py
+++ servicio-2/app.py
@@ -4,56 +4,46 @@ from influxdb import InfluxDBClient
 from datetime import datetime
 import json
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb import InfluxDBClient
-#from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-token = "my-token"
+token = "my-admin-token"
 org = "tecnoandina"
 bucket = "system"
-
-# #Setup database
-# client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'mydb')
-# client.create_database('mydb')
-# client.get_list_database()
-# client.switch_database('mydb')
-
+url = "http://influx:8086"
 
 def send_data_influx_db(json_payload):
-    # with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
-    client = InfluxDBClient('influx', '8086', 'admin', 'admin', 'system')
- 
-    #client.create_database('system')
-    # print("parte2")
-    #client.get_list_database()
-    # print("parte3")
-    #client.switch_database('system')
-    # print("parte4")
-    # #write_api = client.write_api(write_options=SYNCHRONOUS)
+    with InfluxDBClient(url=url, token=token, org=org) as client:   
+        payload = json.loads(json_payload)
+        # senddata={}
+        # senddata["measurement"] = "test06"
+        # senddata["measurement"] = "test06"
+        # senddata["tags"] = {}
+        # senddata["tags"]["version"] = payload['version']
+        # senddata["fields"]={}
+        # senddata["fields"]["time"] = payload['time']
+        # senddata["fields"]["value"] = str(payload['value'])
+        point = Point("Test06") \
+            .tag("version", payload['version']) \
+            .field("time", payload['time']) \
+            .field("value", payload['value'])
 
-    payload = json.loads(json_payload)
-    # print("parte5")
-
-    # point = Point("Test04") \
-    #     .tag("version", payload['version']) \
-    #     .field("time", payload['time']) \
-    #     .field("value", str(payload['value']))
-    
-    # print("parte6")
-
-    # print("data", point)
-    # write_api.write(bucket, org, point)
+        print("data", point)
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write(bucket=bucket, record=point)
 
 def on_connect(client, userdata, flags, rc):
     print("Se conecto con mqtt " + str(rc))
     client.subscribe("challenge/dispositivo/#")
     pass
 
+
 def on_message(client, userdata, msg):
     if msg.topic == "challenge/dispositivo/rx":
         print(msg.payload)
         send_data_influx_db(msg.payload)
     pass
-        
+
+
 def main():
     try:
         client = mqtt.Client()
